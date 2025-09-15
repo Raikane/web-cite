d3f23d6589f8d1de23aa612e3aaadb646cd94975
ibrahim diao <bestcan2023@gmail.com>
2025-09-15 01:31:44 +0000

    deuxieme commit

--- script.js
+++ script.js
@@ -32,20 +32,5 @@ const inputs = document.querySelectorAll(".input");
   parent.classList.add("focus");
 };
 
- function blurFunc () {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-};
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
-   
-   
- 
-
  
 
